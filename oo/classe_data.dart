d44d43643381edc2f3a26b23a5047441b91a4ac7
Richard Borges <richard.borges.cc@gmail.com>
2023-10-26 14:25:27 -0300

    chore: class methods

--- oo/classe_data.dart
+++ oo/classe_data.dart
@@ -2,6 +2,14 @@ class Data {
   int dia = 1;
   int mes = 1;
   int ano = 1;
+
+  String obeterFormatada() {
+    return "$dia/$mes/$ano";
+  }
+
+  String toString() {
+    return obeterFormatada();
+  }
 }
 
 void main(List<String> args) {
@@ -10,12 +18,18 @@ void main(List<String> args) {
   dataAniversario.mes = 10;
   dataAniversario.ano = 2020;
 
-  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  String d1 = dataAniversario.obeterFormatada();
+  print("A data do aniversário é $d1");
 
   Data dataCompra = Data();
   dataCompra.dia = 11;
   dataCompra.mes = 12;
   dataCompra.ano = 2021;
 
-  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+  // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+  String d2 = dataCompra.obeterFormatada();
+  print("A data da compra é $d2");
+  print(dataCompra);
+  print(dataCompra.toString());
 }
