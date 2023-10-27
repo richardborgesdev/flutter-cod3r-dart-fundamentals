ae6ee9e6c1c3c45d151b70d553220abd0bcbad18
Richard Borges <richard.borges.cc@gmail.com>
2023-10-27 11:21:46 -0300

    chore: class constructor

--- oo/classe_data.dart
+++ oo/classe_data.dart
@@ -1,7 +1,15 @@
 class Data {
-  int dia = 1;
-  int mes = 1;
-  int ano = 1;
+  int dia;
+  int mes;
+  int ano;
+
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
 
   String obeterFormatada() {
     return "$dia/$mes/$ano";
@@ -13,23 +21,28 @@ class Data {
 }
 
 void main(List<String> args) {
-  var dataAniversario = new Data();
-  dataAniversario.dia = 3;
-  dataAniversario.mes = 10;
-  dataAniversario.ano = 2020;
+  var dataAniversario = new Data(3, 10, 2020);
+  // dataAniversario.dia = 3;
+  // dataAniversario.mes = 10;
+  // dataAniversario.ano = 2020;
 
   // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
   String d1 = dataAniversario.obeterFormatada();
   print("A data do aniversário é $d1");
 
-  Data dataCompra = Data();
-  dataCompra.dia = 11;
-  dataCompra.mes = 12;
-  dataCompra.ano = 2021;
+  Data dataCompra = Data(11, 12, 2021);
+  // dataCompra.dia = 11;
+  // dataCompra.mes = 12;
+  // dataCompra.ano = 2021;
 
   // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
   String d2 = dataCompra.obeterFormatada();
   print("A data da compra é $d2");
   print(dataCompra);
   print(dataCompra.toString());
+
+  print(new Data());
+  print(Data(31));
+  print(Data(31, 12));
+  print(Data(31, 12, 2021));
 }
