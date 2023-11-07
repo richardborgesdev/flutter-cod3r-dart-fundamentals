785a2843248aba27120d02a8f3f3f8540f0f002a
Richard Borges <richard.borges.cc@gmail.com>
2023-11-07 17:04:29 -0300

    chore: [class] composition example

---
+++ oo/modelo/venda.dart
@@ -0,0 +1,18 @@
+import 'cliente.dart';
+import 'venda_item.dart';
+
+class Venda {
+  Cliente cliente;
+  List<VendaItem> itens;
+
+  Venda({
+    this.cliente,
+    this.itens = const [],
+  });
+
+  double get valorTotal {
+    return itens
+        .map((item) => item.preco * item.quantidade)
+        .reduce((total, item) => total + item);
+  }
+}
---
+++ oo/modelo/venda_item.dart
@@ -0,0 +1,26 @@
+import 'produto.dart';
+
+class VendaItem {
+  int quantidade;
+  double _preco;
+  Produto produto;
+
+  VendaItem({
+    this.produto,
+    this.quantidade = 1,
+  });
+
+  double get preco {
+    if (produto != null && _preco == null) {
+      _preco = produto.precoComDesconto;
+    }
+
+    return _preco;
+  }
+
+  void set preco(double novoPreco) {
+    if (novoPreco > 0) {
+      _preco = novoPreco;
+    }
+  }
+}
